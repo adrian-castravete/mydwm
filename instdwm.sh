b24c8fa92f1dd508bedb5be0ef250b8e31d8adb9
Adrian Castravete <fkbm@lenovo-H0M3.frozenknight.com>
2020-03-12 13:48:59 +0200

    Add dbus-launch and prepare for other scripts

--- instdwm.sh
+++ instdwm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #sudo apt install git openssh-client ssh
-sudo apt install lua5.1 build-essential gcc libx11-dev libxft-dev libxinerama-dev
+sudo apt install -y lua5.1 build-essential gcc libx11-dev libxft-dev libxinerama-dev dbus-x11
+sudo apt install -y vim-gtk sxhkd lxterminal lxtask xfce4-power-manager xfce4-screenshooter
 
 CWD_PATH=`pwd`
 git submodule update --init --recursive
@@ -14,3 +15,6 @@ for filename in $(ls $CWD_PATH/home-scripts)
 do
   cp "$CWD_PATH/home-scripts/$filename" "$HOME/.$filename"
 done
+
+mkdir -p $HOME/.local/bin
+cp "$CWD_PATH/other-scripts/sxhkd" "$HOME/.config/sxhkd/"
